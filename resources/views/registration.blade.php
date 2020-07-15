a2563fa164771c15df5c21c1b4199bf87d465c41
Sam Achola <sam.achola@live.com>
2020-07-15 17:26:45 +0300

    Implement layout

---
+++ resources/views/registration.blade.php
@@ -0,0 +1,7 @@
+@extends ('layout')
+@section ('content')
+
+<div class="registratiion">
+    <h1>Register</h1>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    $name = request('name');
+    return view('test', [
+        'name' => $name,
+    ]);
+});
+
+Route::get('/register', 'AuthController@get');
+
+Route::get('/projects/{projectId}', 'ProjectsController@get');
